fd7121e5dc030a2768a4646bb3d6115fc3bb907b
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-08 11:47:19 -0800

    fetch from supabase

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,20 @@
 // all films, single film(id), single character(film_id)
+import { client } from './client';
+
+export async function getGhibliFilms() {
+  const response = await client
+    .from('films')
+    .select();
+
+  return response(data);
+}
+
+export async function getSingleFilm(id) {
+  const response = await client
+    .from('films')
+    .select()
+    .match({ id })
+    .single();
+
+  return response(data);
+}

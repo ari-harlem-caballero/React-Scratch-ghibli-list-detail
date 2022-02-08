3f7bdd288eb3e74d87e83ca973568d67bffb46e1
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-08 11:43:50 -0800

    created single film file

--- src/Components/Films/Film.js
+++ src/Components/Films/Film.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Film() {
-  return <div>
+export default function Film({ ghibliFilm }) {
+  return (
+  <Link to={`/film/${ghibliFilm.id}`}>
+    <div className='film'>
     {/* links to detail page....holds img/title, dir, date, run, description */}
-  </div>;
+      <img src={ghibliFilm.image}/>
+      <h1>{ghibliFilm.title}</h1>
+      <p className='film-info'>{ghibliFilm.director} -- {ghibliFilm.release_date}, {ghibliFilm.run_time}</p>
+      <p className='description'>{ghibliFilm.description}</p>
+    </div>;
+  </Link>
+  );
 }

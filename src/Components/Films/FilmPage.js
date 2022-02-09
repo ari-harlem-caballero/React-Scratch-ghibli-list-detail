5c2a72274e311e92bdae27f534d4f1b0d7a3e5fa
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-08 21:30:28 -0800

    functioning search query

--- src/Components/Films/FilmPage.js
+++ src/Components/Films/FilmPage.js
@@ -1,18 +1,15 @@
 import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 import { getGhibliFilms } from '../../services/fetch-utils';
 import FilmList from './FilmList';
 
 function FilmPage() {
   const [ghibliFilms, setGhibliFilms] = useState([]);
   const [filterQuery, setFilterQuery] = useState('');
-  // const [filteredFilms, setFilteredFilms] = useState([]);
   const [page, setPage] = useState(1);
   const perPage = 6;
 
   useEffect(() => {
     async function fetchPage() {
-      // const magicNumber = page !== 1 && 1;
       const from = page * perPage - perPage;
       const to = page * perPage - 1;
       const films = await getGhibliFilms(from, to, filterQuery);
@@ -23,13 +20,9 @@ function FilmPage() {
     fetchPage();
   }, [page, filterQuery]);
 
-  // useEffect(() => {
-  //   const filteredGhibliFilms = ghibliFilms.filter()
-  // }, []);
 
   return (
     <>
-      {/* <Link to="/">Home</Link> */}
       <h4>Current Page: {page}</h4>
       <div className='buttons'>
         {/* setPage, buttons, holds FilmList */}
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,14 +1,25 @@
 // all films, single film(id), single character(film_id)
 import { client } from './client';
 
-export async function getGhibliFilms(from = 1, to = 6) {
-  const response = await client
-    .from('films')
-    .select()
-    .order('id', { ascending: true })
-    .range(from, to);
+export async function getGhibliFilms(from = 1, to = 6, filterQuery) {
+  
+  if (!filterQuery) {
+    const response = await client
+      .from('films')
+      .select()
+      .order('id', { ascending: true })
+      .range(from, to);
 
-  return response.data;
+    return response.data;
+  } else {
+    const response = await client
+      .from('films')
+      .select()
+      .range(from, to)
+      .ilike('title', `%${filterQuery}%`);
+
+    return response.data;
+  }
 }
 
 export async function getSingleFilm(id) {
@@ -20,12 +31,3 @@ export async function getSingleFilm(id) {
 
   return response.data;
 }
-
-// export async function getCharacters(film_id) {
-//   const response = await client
-//     .from('characters')
-//     .select(`*, films (*)`)
-//     .match({ film_id });
-
-//   return response.data;
-// }

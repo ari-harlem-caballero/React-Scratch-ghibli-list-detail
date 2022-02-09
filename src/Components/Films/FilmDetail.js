6d50955480473a997367c8813479ac9beba5911e
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-08 18:44:46 -0800

    functioning app, still working on characters

--- src/Components/Films/FilmDetail.js
+++ src/Components/Films/FilmDetail.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { getSingleFilm } from '../../services/fetch-utils';
+// import CharactersList from '../Characters/CharactersList';
+
 
 export default function FilmDetail() {
   const [ghibliFilm, setGhibliFilm] = useState('');
@@ -27,6 +29,7 @@ export default function FilmDetail() {
         <h1>{ghibliFilm.title}</h1>
         <p className='film-info'><span className='director'>{ghibliFilm.director}</span> -- {ghibliFilm.release_date}, {ghibliFilm.run_time} mins</p>
         <p className='description'>{ghibliFilm.description}</p>
+        {/* <CharactersList characters={characters} /> */}
       </div>
     </>
   );
